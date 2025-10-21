dd8a4fd1830e432f9d0ae6c981a0867ebee30aba
Gulkaiyr/Guka <gulkaiyr@raincheck.co>
2025-10-21 15:01:21 +0600

    Initial Spring Boot project with HelloController

---
+++ src/main/java/com/edvo/EdvoApplication.java
@@ -0,0 +1,13 @@
+package com.edvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdvoApplication.class, args);
+	}
+
+}
