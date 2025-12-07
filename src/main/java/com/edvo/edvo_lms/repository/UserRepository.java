73315508f725d0f270390f6f1fc64dc867b445ce
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-07 15:13:02 +0600

    Add UserRepository with findByUsername and existsByUsername

--- src/main/java/com/edvo/edvo_lms/repository/UserRepository.java
+++ src/main/java/com/edvo/edvo_lms/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.edvo.edvo_lms.repository;
 
 import com.edvo.edvo_lms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existByUsername(String username);
+
 }
