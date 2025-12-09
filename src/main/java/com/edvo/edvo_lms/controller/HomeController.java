96a017aa5bbce2eb601ade5bf704df842a95cf5a
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-09 16:02:09 -0500

    feat(home): add HomeController with root endpoint to verify Spring Boot is running

--- src/main/java/com/edvo/edvo_lms/controller/HomeController.java
+++ src/main/java/com/edvo/edvo_lms/controller/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
     @GetMapping("/")
-    public String home(){
-        return  "Spring Boot is running!";
+    public String home() {
+        return "Hello Edvo!";
     }
 }
